b32a9e3dda7da953893cf7069fc9fe8509594c81
TheCosmere64 <narutorules@hotmail.com.au>
2017-05-10 13:28:51 +1000

    s

--- src/asgn2Customers/Customer.java
+++ src/asgn2Customers/Customer.java
@@ -11,7 +11,16 @@ import asgn2Exceptions.CustomerException;
  * @author Person B
 */
 public abstract class Customer {
+<<<<<<< HEAD
 
+=======
+	
+	private String custName;
+	private String custMobileNum;
+	private int custX;
+	private int custY;
+	private String custType;
+>>>>>>> parent of 27f4938... hm
 
 	/**
 	 *  This class represents a customer of the Pizza Palace restaurant.  A detailed description of the class's fields
